d6169936f7b846df62fb44a852e8ccfda1213c46
tjhlp <374586186@qq.com>
2019-08-07 17:17:10 +0800

    添加条形图的数字标注功能

--- BossSpider/data/handler.py
+++ BossSpider/data/handler.py
@@ -6,17 +6,18 @@ import jieba
 import time
 from multiprocessing import Pool
 import matplotlib.pyplot as plt
+from config import TEST_COMPANY
 
 jieba.load_userdict("userdict.txt")
 POOL_NUMBER = 2
-po = Pool(POOL_NUMBER)  # 定义一个进程池，最大进程数2
+
 
 def count_time(func):
     """计时装饰器"""
 
-    def inner(self,*args, **kwargs):
+    def inner(self, *args, **kwargs):
         start_time = time.time()
-        result = func(self,*args, **kwargs)
+        result = func(self, *args, **kwargs)
         end_time = time.time()
         print('处理数据用时:{}'.format(end_time - start_time))
         return result
@@ -28,6 +29,7 @@ class HandlerData(object):
     def __init__(self, file):
         self.file = file
         self.remove_list = []
+        self.job_info_file = []
         self._load_stopwords()
         self.words_valid_dict = {}
 
@@ -58,7 +60,6 @@ class HandlerData(object):
         :param : 文件夹
         :return:所有职业信息列表
       """
-        job_info_file = []
         paths = os.listdir(self.file)
         for path in paths:
             if re.match(r'2019', path):
@@ -66,21 +67,16 @@ class HandlerData(object):
                     content = json.loads(f.read())
                     for job_info in content:
                         if job_info['job_name']:
-                            job_info_file.append(job_info)
-        return job_info_file
+                            self.job_info_file.append(job_info)
 
     # {'job_name': 'Python高级工程师', 'job_salary': '25-50K', 'job_text': '职位要求：\n1、负责外部公',
     # 'company_name': '深圳市星商电子商务有限公司', 'job_location': '深圳龙岗区云里智能园4栋2楼'}
 
-    @count_time
-    def data_classification(self, job_info_file):
-        """分类数据"""
+    def company_classification(self):
+        """统计公司数据"""
         company_dict = {}
-        for job_comp in job_info_file:
-            a = job_comp.get('job_text')
-            participle_data = jieba.lcut(a)
+        for job_comp in self.job_info_file:
             company_name = job_comp.get('company_name')
-            process_data1 = self.count_words(participle_data)
             if company_name in company_dict:
                 company_dict[company_name] += 1
             else:
@@ -89,46 +85,65 @@ class HandlerData(object):
         return company_dict
 
     @staticmethod
-    def plot_data(company_data, filename):
+    def job_info_classification(job_div_info):
+        """统计职业信息"""
+        participle_data = []
+        for job_comp in job_div_info:
+            a = job_comp.get('job_text')
+            participle_data = jieba.lcut(a)
+        return participle_data
+
+    @count_time
+    def multi_job(self):
+        """多进程解析数据"""
+        # TODO 多进程 解析数据
+        result = []
+        po = Pool(POOL_NUMBER)  # 定义一个进程池，最大进程数2
+        get_size = len(self.job_info_file)
+        for index in range(POOL_NUMBER):
+            data_size_back = int(get_size / POOL_NUMBER * index)
+            data_size_forward = int(get_size / POOL_NUMBER * (index - 1))
+            divided_data = self.job_info_file[data_size_forward:data_size_back]
+            res = po.apply_async(self.job_info_classification, (divided_data,))
+            print(res)
+            # result.append()
+        po.close()
+        po.join()
+
+    @staticmethod
+    def plot_data(company_data, filenames):
         """
         将信息（公司与职位信息）可视化
         :param company_data: （公司与职位数量）以键值对信息传入
+        :param filenames: 保存文件的位置
         :return: None
         """
         company_list = []
-        value_list = []
+        y_core = []
         for key, value in company_data.items():
             company_list.append(key)
-            value_list.append(value)
+            y_core.append(value)
         plt.figure(figsize=(20, 8), dpi=80)
         plt.rcParams['figure.figsize'] = (15.0, 8.0)  # 显示大小
         x_cor = [x for x in range(len(company_list))]
-        plt.barh(x_cor, value_list, facecolor='orange', height=0.3, label='职位数量')
+        plt.barh(x_cor, y_core, facecolor='orange', height=0.3, label='职位数量')
+        # 添加数字标号
+        for score, pos in zip(y_core, x_cor):
+            plt.text(score + 2, pos, '%d' % score)
         plt.yticks(x_cor, company_list)
         plt.grid(alpha=0.3)
         plt.xlabel('公司招聘职位')
-        plt.savefig(filename)
+        plt.savefig(filenames)
         plt.legend(loc='best')
         plt.show()
 
     def run(self):
         # 读取数据
-        job_info_file = self.read_json()
-        pr_company_data = self.data_classification(job_info_file)
+        self.read_json()
+        pr_company_data = self.company_classification()
         company_data = sorted(pr_company_data.items(), key=lambda x: x[1], reverse=True)
         res_company_data = dict(company_data[:9])
         self.plot_data(res_company_data, './company.jpg')
-
-        # 多进程 解析数据
-        # result = []
-        # for index in range(POOL_NUMBER):
-        #     result.append(po.apply_async(self.data_classification, (job_info_file,)))
-        #
-        # print(len(company_data))
-        # print(company_data)
-        # po.close()  # 关闭进程池，关闭后po不再接收新的请求
-        # po.join()
-
 
         # 取出计数排名前10的元素
         # words_data = sorted(self.words_valid_dict.items(), key=lambda x: x[1], reverse=True)
@@ -140,5 +155,4 @@ class HandlerData(object):
 if __name__ == '__main__':
     hd = HandlerData('../data')
     hd.run()
-
-
+    # hd.plot_data(dict(TEST_COMPANY[:20]), './company.jpg')
